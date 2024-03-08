dc37af06dc484106b14868331b3523542dd4a6ac
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-03-08 01:25:15 -0300

    Adjusting reactive calculator

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,37 @@
 <script setup>
-  import { reactive, ref } from 'vue';
+  import { reactive } from 'vue';
+  import Title from './components/Title.vue'
+  import Math from './components/Math.vue'
+  import Res from './components/Res.vue'
 
   const calculadora = reactive({
-    numero1: null,
-    numero2: null,
-    operacao: 'soma',
-  });
+    operation: '+',
+    numero1: '',
+    numero2: ''
+  })
 
-  const done = ref(null);
-
-  const calcularResultado = () => {
-    const num1 = parseFloat(calculadora.numero1);
-    const num2 = parseFloat(calculadora.numero2);
-    
-    switch (calculadora.operacao) {
-      case 'soma':
-        return num1 + num2;
-      case 'subtracao':
-        return num1 - num2;
-      case 'multiplicacao':
-        return num1 * num2;
-      case 'divisao':
-        return num1 / num2;
-      default:
-          return null;
+  const resultado = () => {
+    const {operation} = calculadora
+    switch (operation) {
+      case '-':
+        return parseFloat(calculadora.numero1) - parseFloat(calculadora.numero2)
+      case '*':
+        return parseFloat(calculadora.numero1) * parseFloat(calculadora.numero2)
+      case '/':
+        return parseFloat(calculadora.numero1) / parseFloat(calculadora.numero2)
+      default: 
+        return parseFloat(calculadora.numero1) + parseFloat(calculadora.numero2)
     }
   }
-
-  const atualizarResultado = () => {
-    done.value = calcularResultado();
-  }
-;
 </script>
 
 <template>
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-    <h1>Operação Aritmética</h1>
-    <form @input="atualizarResultado">
-      <div class="row">
-        <div class="col-md-2 mt-4">
-          <input v-model="calculadora.numero1" type="number" class="form-control mb-2" placeholder="Insira um número aqui">
-          <input v-model="calculadora.numero2" type="number" class="form-control" placeholder="Insira um número aqui">
-        </div>
-        <div class="col-md-2 mt-4">
-          <select v-model="calculadora.operacao" class="form-control">
-            <option value="soma">Soma</option>
-            <option value="subtracao">Subtração</option>
-            <option value="multiplicacao">Multiplicação</option>
-            <option value="divisao">Divisão</option>
-          </select>
-          <h2 class="mt-2 ms-2">O resultado é {{ done }}</h2>
-        </div>
-      </div>
-    </form>
-  </header>
+  <body>
+    <div class="container">
+      <Title></Title>
+      <Math :operacao="evento => calculadora.operation = evento.target.value" :numero1="evento => calculadora.numero1 = evento.target.value" :numero2="evento => calculadora.numero2 = evento.target.value"></Math>
+      <Res :numero1="calculadora.numero1" :numero2="calculadora.numero2" :result="resultado()" ></Res>
+    </div>
+  </body>
 </template>
 
-  <style scoped>
-  .done {
-    text-decoration: line-through;
-  }
-  </style>
---
+++ src/components/Math.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps(['numero1', 'numero2', 'operacao'])
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-md-2 mt-4">
+            <input @keyup="props.numero1" type="number" required class="form-control mb-2"
+                placeholder="Insira um número aqui">
+            <input @keyup="props.numero2" type="number" required class="form-control"
+                placeholder="Insira um número aqui">
+        </div>
+        <div class="col-md-2 mt-4">
+            <select @change="props.operacao" class="form-control">
+                <option value="+">Somar</option>
+                <option value="-">Subtrair</option>
+                <option value="*">Multiplicar</option>
+                <option value="/">Divisão</option>
+            </select>
+        </div>
+    </div>
+</template>
